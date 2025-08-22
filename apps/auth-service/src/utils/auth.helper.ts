1707b74c71877415c995fd9e4387f429484803f4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-08-22 18:30:33 +0600

    chore: update .gitignore and package configs, refactor error-handler
    - Add .env to .gitignore to prevent sensitive data from being tracked
    - Update bun.lock and package.json for dependency consistency
    - Refactor error-handler/index.ts for improved error management
    - Add controllers and utils to auth-service for better code organization
    - Add libs package for shared Prisma client and utilities

---
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -0,0 +1,23 @@
+import { ValidationError } from "../../../../packages/error-handler";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const validateRegistrationData = (
+  data: any,
+  userType: "user" | "seller"
+) => {
+  const { name, email, password, phone_number, country } = data;
+
+  if (
+    !name ||
+    !email ||
+    !password ||
+    (userType === "seller" && (!phone_number || !country))
+  ) {
+    throw new ValidationError(`Missing required Fields!`);
+  }
+
+  if (!emailRegex.test(email)) {
+    throw new ValidationError("Invalid Email Format!");
+  }
+};
